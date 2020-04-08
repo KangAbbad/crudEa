37f59d9a071d49b5ad130e57a454fa622d3c5724
kangabbad <naufalabbad9@gmail.com>
2020-04-08 08:49:21 +0700

    using online api

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ class App extends Component {
   }
 
   onGetDataSantri = () => {
-    axios.get('http://localhost:4000/posts?_sort=id&_order=desc')
+    axios.get('https://my-json-server.typicode.com/KangAbbad/crudEa/posts?_sort=id&_order=desc')
       .then((result) => {
         this.setState({
           dataSantri: result.data
